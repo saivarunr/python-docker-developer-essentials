04f77b5e78bc2204046174d26687979905ca022a
Sai Varun Reddy Daram <saivarunvishal@gmail.com>
2018-10-12 11:19:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Python Docker Developer Essentials
+
+Python docker developer essentials install the following
+
+  - Docker
+  - Docker-compose
+  - Sublime
+  - Pycharm
+
+Installation steps
+
+  - git clone this package
+  - cd into this package
+  - sudo chmod 700 install.sh
+  - RUN ./install.sh
---
+++ install.sh
@@ -0,0 +1,18 @@
+sudo apt-get -y update
+sudo apt-get install -y docker.io
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get -y update
+sudo apt-get -y install docker-ce
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get -y update
+sudo apt-get install -y snap
+sudo snap install pycharm-community -classic
+
+echo "Installation done Please logout and login back"
