382f609c2ab616ba949aaf259158066623aaa43f
Sai Varun Reddy Daram <saivarunvishal@gmail.com>
2018-10-12 11:22:18 +0530

    MiscS

--- install.sh
+++ install.sh
@@ -13,6 +13,6 @@ wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get -y update
 sudo apt-get install -y snap
-sudo snap install pycharm-community -classic
+sudo snap install pycharm-community --classic
 
 echo "Installation done Please logout and login back"
